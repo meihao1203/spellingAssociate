d0f157b1cb15350c28a43be68864086d6443c8f5
meihao1203 <meihao19931203@outlook.com>
2018-05-16 16:50:45 +0800

    Index.h

---
+++ offline/src/Index/Index.cpp
@@ -0,0 +1,18 @@
+ ///
+ /// @file    Index.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-16 16:48:21
+ ///
+ 
+#include"Index.h"
+#include<iostream>
+using namespace std;
+namespace meihao
+{
+	Index::Index(const string& path):_path(path)
+	{
+	}
+	void Index::readDict()
+	{
+	}
+};
--- offline/src/Index/Index.h
+++ offline/src/Index/Index.h
@@ -16,13 +16,14 @@ namespace meihao
 	{
 		public:
 			Index(const string& path);  //传递dict.txt路径
+		private:  // 内部处理函数，不提供对外接口
 			void readDict();
 			void indexMap();
 			void indexMapSave();  // 建立好的索引存入文件
 		private:
-			vector<pair<string,int>> _readDict;  // dict.txt文件都是string int格式，
+			vector<pair<string,int> > _readDict;  // dict.txt文件都是string int格式，
 			// 这里全部按这个格式一行一行的读进来
-			map<char,set<int>> _index;  // 每个字符，统计在dict.txt中出现的位置和行号
+			map<char,set<int> > _index;  // 每个字符，统计在dict.txt中出现的位置和行号
 			// 行号就等于_readDict对应单词的下标
 			string _path;
 	};
