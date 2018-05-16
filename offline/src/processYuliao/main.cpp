ef592fcca243623f2d9c5f7d54ec1bcf57ce11f4
meihao1203 <meihao19931203@outlook.com>
2018-05-16 16:28:09 +0800

    processYuliao/

---
+++ offline/src/processYuliao/main.cpp
@@ -0,0 +1,18 @@
+ ///
+ /// @file    main.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-15 16:04:06
+ ///
+ 
+#include"processDict.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	meihao::processDict pa("./../../source/");
+	//meihao::processDict pa("./file1.txt");
+	pa.showFile();
+	cout<<"start process ..."<<endl;
+	pa.process();
+	cout<<"end process !"<<endl;
+}
