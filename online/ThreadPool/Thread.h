fee475b6a9b7d6557475dbe656e16977a732b130
meihao1203 <meihao19931203@outlook.com>
2018-05-17 09:58:15 +0800

    Thread.h

---
+++ online/ThreadPool/Thread.h
@@ -0,0 +1,15 @@
+ ///
+ /// @file    Thread.h
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-17 09:56:27
+ ///
+ 
+#ifndef __THREAD_H__
+#define __THREAD_H__
+#include<iostream>
+using namespace std;
+namespace meihao
+{
+	class Thread
+};
+#endif
