1ac45c8624440c375fb0b4edeee8a4b6b6d1206e
meihao1203 <meihao19931203@outlook.com>
2018-05-17 11:01:27 +0800

    Thread.h

--- online/ThreadPool/Thread.h
+++ online/ThreadPool/Thread.h
@@ -7,9 +7,23 @@
 #ifndef __THREAD_H__
 #define __THREAD_H__
 #include<iostream>
+#include"Noncopyable.h"
+#include<pthread.h>
 using namespace std;
 namespace meihao
 {
-	class Thread
+	class Thread:private Noncopyable //防止Thread被拷贝，私有继承，防止Noncopyable指向派生类对象
+	{
+		public:
+			Thread();
+			void start();
+			void join();
+			virtual void run() = 0;
+			virtual ~Thread();
+			static void* threadFunc(void*);
+		private:
+			pthread_t _pthId;
+			bool _isRunning;
+	};
 };
 #endif
