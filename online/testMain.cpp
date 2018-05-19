7c9c4cab8727bd3aeecd121b81ef32d79ea39824
meihao1203 <meihao19931203@outlook.com>
2018-05-19 16:35:19 +0800

    testMain.cpp

--- online/testMain.cpp
+++ online/testMain.cpp
@@ -5,10 +5,16 @@
  ///
  
 #include"MyConf.h"
+#include"MyDict.h"
 #include<iostream>
 using namespace std;
 int main()
 {
 	meihao::MyConf conf("/home/meihao/spellingAssociate/online/conf/sa.conf");
 	conf.show();
+	meihao::MyDict* dict = meihao::MyDict::getInstance();
+	dict->init( (conf.getMap())["dict.txt"].c_str(),NULL ) ;
+	//dict->show_dict();
+	dict->write_index_table();
+	return 0;
 }
