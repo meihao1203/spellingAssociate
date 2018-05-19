fb49092fc74968ff80be3d5f65fa58da4540f77e
meihao1203 <meihao19931203@outlook.com>
2018-05-19 11:13:40 +0800

    Myconf.cpp

---
+++ online/MyConf.cpp
@@ -0,0 +1,17 @@
+ ///
+ /// @file    MyConf.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-19 11:12:40
+ ///
+ 
+#include"MyConf.h"
+#include<iostream>
+using namespace std;
+namespace meihao
+{
+	MyConf::MyConf(const string& path)
+	{
+		_filepath = path;
+	}
+
+};
