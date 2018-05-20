45c34eca9734641f829e25857cef308c6660fbc3
meihao1203 <meihao19931203@outlook.com>
2018-05-20 10:35:39 +0800

    test.cpp

--- online/ThreadPool/test.cpp
+++ online/ThreadPool/test.cpp
@@ -4,35 +4,31 @@
  /// @date    2018-05-16 19:40:26
  ///
  
-#include"MutexLock.h"
 #include"Threadpool.h"
-#include"myTask.h"
-#include"Task.h"
 #include<iostream>
 #include<unistd.h>
+#include"Cache.h"
 using namespace std;
+void threadrun(meihao::Cache& cache)
+{
+	cout<<cache.query("mei")<<endl;
+	cout<<cache.query("张")<<endl;
+	cache.addElement("张","张三");
+}
 int main()
 {
-	meihao::Threadpool threadpool(4,10);  //4线程，10任务
+	meihao::MyConf myconf("/home/meihao/spellingAssociate/online/conf/sa.conf");
+	meihao::Threadpool threadpool(4,10,myconf);  //4线程，10任务
 	threadpool.start();
-	meihao::Task* task = new meihao::myTask;
 	int cnt = 10;
 	while(cnt>0)
 	{
-		threadpool.addTask(task);
+		threadpool.addTask(threadrun);
 		sleep(1);  //这里不睡眠，线程执行太快了，myTask种子是用系统时间，导致数据都一样
 		cnt--;
 	}
+	threadpool.updateCache();
+	threadpool.addTask(threadrun);
 	threadpool.stop();
 	return 0;
 }
-//produce a num:13
-//produce a num:62
-//produce a num:46
-//produce a num:96
-//produce a num:93
-//produce a num:8
-//produce a num:93
-//produce a num:69
-//produce a num:93
-//produce a num:32
