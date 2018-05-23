5d5b729ee815135b55a8f6679541031785310b7a
meihao1203 <meihao19931203@outlook.com>
2018-05-23 17:04:09 +0800

    DictConf/

---
+++ online/DictConf/testMain.cpp
@@ -0,0 +1,26 @@
+ //
+ /// @file    testMain.cpp
+ /// @author  meihao1203(meihao19931203@outlook.com)
+ /// @date    2018-05-19 11:31:52
+ ///
+ 
+/* 最开始测试MyConf类和MyDict类的 */
+#include"MyConf.h"
+#include"MyDict.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+	meihao::MyConf conf("/home/meihao/spellingAssociate/online/conf/sa.conf");
+	conf.show();
+	meihao::MyDict* dict = meihao::MyDict::getInstance();
+	dict->init( (conf.getMap())["dict.txt"].c_str(),NULL ) ;
+	//dict->show_dict();
+	dict->write_index_table();
+	return 0;
+}
+//路径 /home/meihao/spellingAssociate/online/conf/sa.conf
+//cache /home/meihao/spellingAssociate/online/conf/cache
+//dict.txt /home/meihao/spellingAssociate/offline/doc/dict.txt
+//ip 192.168.254.136
+//port 8848
