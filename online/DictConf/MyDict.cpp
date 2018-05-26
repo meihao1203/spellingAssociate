af43c507cfba08832f9342316f35580ab33096c7
meihao1203 <meihao19931203@outlook.com>
2018-05-26 21:34:44 +0800

    DictConf/MyDict.cpp

--- online/DictConf/MyDict.cpp
+++ online/DictConf/MyDict.cpp
@@ -41,6 +41,7 @@ namespace meihao
 			 iss>>pairString>>pairInt;
 			 _dict.push_back(::make_pair(pairString,pairInt));
 		 }
+		 ifs.close();
 	 }
 	 void MyDict::record_to_index(int idx)
 	 {
@@ -48,19 +49,55 @@ namespace meihao
 		 string key;
 		 for(int iidx=0;iidx!=word.size();++iidx)
 		 {
-			 key = word[iidx];
-			 _index_table[key].insert(idx);  //不能把直接写word[iidx],要先用一个key来保存
+		// 这里是最先只处理英文的
+		//	 key = word[iidx];
+		//	 _index_table[key].insert(idx);  //不能把直接写word[iidx],要先用一个key来保存
+			char ch = word[iidx];  //先取出第一个字节，判断是英文或一个字节的中文
+			//还是多个字节表示的一个中文
+			if(ch&(1<<7))
+			{
+				if((ch&0xE0)==0xC0)  //两个字节
+				{
+					key = word.substr(iidx,2);
+					++idx;  //往后多取了一个字节,要++
+				}
+				else if((ch&0xF0)==0xE0)  //三个字节
+				{
+					key = word.substr(iidx,3);
+					iidx += 2;
+				}
+				else if((ch&0xF8)==0xF0)  //四个字节
+				{
+					key = word.substr(iidx,4);
+					iidx += 3;
+				}
+				else if((ch&0xFC)==0xF8)  //五个字节
+				{
+					key = word.substr(iidx,5);
+					iidx += 4;
+				}
+				else if((ch&0xFE)==0xFC)  //六个字节
+				{
+					key = word.substr(iidx,6);
+					iidx += 5;
+				}
+			}
+			else //英文或单字节的汉字
+			{
+				key = word.substr(iidx,1);
+			}
+			 _index_table[key].insert(idx);
 		 }
 	 }
 	 void MyDict::init(const char* dictpath,const char* cnDictpath)
 	 {
 		 //读入词典文件
 		 read_from(dictpath);
-		 //read_from(cnDictpath);
+		 read_from(cnDictpath);
 		 //建立索引
 		 for(int idx=0;idx!=_dict.size();++idx)
 		{
-			record_to_index(idx);
+			record_to_index(idx);  //对字典中的每一行的每一个单词建立索引
 		}
 	 }
 	 void MyDict::show_dict()
