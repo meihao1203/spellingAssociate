e4d9f7bacd65cb6bf2acc6fed2ea5d40bca48721
meihao1203 <meihao19931203@outlook.com>
2018-05-26 11:22:14 +0800

    main.cpp

--- offline/src/processYuliao/main.cpp
+++ offline/src/processYuliao/main.cpp
@@ -9,7 +9,8 @@
 using namespace std;
 int main()
 {
-	meihao::processDict pa("./../../source/");
+	meihao::processDict pa("./../../source/");  //获取目录文件夹下的所有文件，并解析,生成词典
+	//source下只能放英文语料,可以多个文件
 	//meihao::processDict pa("./file1.txt");
 	pa.showFile();
 	cout<<"start process ..."<<endl;
