64b13573b4463a66805c3ea12cc7d0b18424cc2a
meihao1203 <meihao19931203@outlook.com>
2018-05-26 21:37:31 +0800

    EditDistance.h

--- online/EditDistance.h
+++ online/EditDistance.h
@@ -18,6 +18,8 @@ namespace meihao
 			static int min(const int& a,const int& b,const int& c);
 			static int editDistance(const string& source,const string& target);  // 源字符串变成目标字符串需要的编辑距离
 			//void showEditMatrix(int (*editMatrix)[8],int i);
+			static int nbytes(const char ch);   //针对处理中文词典，对每一个中文单词中的每一个汉子建立索引
+			//由于汉字和英文字母不一样，英文字母ASCII码只有一个字节，最高位为0，中文汉子UTF-8编码，最多可以6个字节
 	};
 };
 #endif
